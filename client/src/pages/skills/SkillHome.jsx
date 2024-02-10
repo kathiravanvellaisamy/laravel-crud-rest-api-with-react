4db7e9b9ad56d014543cd479f130e2719920627a
kathiravan vellaisamy <kathiravanvellaisamy@gmail.com>
2024-02-10 15:33:06 +0530

    Added key attribute

--- client/src/pages/skills/SkillHome.jsx
+++ client/src/pages/skills/SkillHome.jsx
@@ -49,7 +49,10 @@ const SkillHome = () => {
               </thead>
               <tbody>
                 {skills.map((skill) => (
-                  <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                  <tr
+                    key={skil.id}
+                    className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700"
+                  >
                     <th
                       scope="row"
                       className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
