0ac260fb7962ab46d395c022cbf77661ba1c2b5d
kathiravan vellaisamy <kathiravanvellaisamy@gmail.com>
2024-02-10 15:47:21 +0530

    Added state values and setter function

--- client/src/pages/skills/CraeteSkill.jsx
+++ client/src/pages/skills/CraeteSkill.jsx
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 const CraeteSkill = () => {
+  const [formValues, setFormValues] = useState({
+    name: "",
+    slug: "",
+  });
+
+  const onChange = () => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log();
+  };
   return (
     <div className="max-w-3xl mx-auto">
       <div className="py-4 bg-slate-100">
@@ -8,21 +22,29 @@ const CraeteSkill = () => {
           Create Skill
         </h1>
       </div>
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <div className="my-1 py-1">
-          <label htmlFor="name">Name</label>
+          <label htmlFor="name" className="font-semibold text-md mt-1">
+            Name
+          </label>
           <input
             type="text"
-            name=""
-            id=""
+            name="name"
+            value={formValues["name"]}
+            onChange={onChange}
+            id="name"
             className="px-3 py-2 mt-1 border border-slate-300 rounded-md w-full"
           />
         </div>
         <div className="my-1 py-1">
-          <label htmlFor="slug">Slug</label>
+          <label htmlFor="slug" className="font-semibold text-md mt-1">
+            Slug
+          </label>
           <input
             type="text"
-            name=""
+            name="slug"
+            value={formValues["slug"]}
+            onChange={onChange}
             id="slug"
             className="px-3 py-2 mt-1 border border-slate-300 rounded-md w-full"
           />
