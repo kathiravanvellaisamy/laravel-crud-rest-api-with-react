5d879b1b7529b48bdf468519e54c8f58626cb51a
kathiravan vellaisamy <kathiravanvellaisamy@gmail.com>
2024-02-10 15:34:10 +0530

    corrected skill spelling

--- client/src/pages/skills/SkillHome.jsx
+++ client/src/pages/skills/SkillHome.jsx
@@ -50,7 +50,7 @@ const SkillHome = () => {
               <tbody>
                 {skills.map((skill) => (
                   <tr
-                    key={skil.id}
+                    key={skill.id}
                     className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700"
                   >
                     <th
